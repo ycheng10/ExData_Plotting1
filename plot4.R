5b9eb6a672df23ed61f911ed8a3137bfb8ec3c68
ycheng10 <yinhahelen@yahoo.com>
2019-02-24 06:07:10 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,7 @@
+# loading dataset file
+hpc <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 
 # Subsetting the data with desired records
 SelData=subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
