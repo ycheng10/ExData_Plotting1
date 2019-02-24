3b2c976d16b97ad2553e0b593d987539c943b3b9
ycheng10 <yinhahelen@yahoo.com>
2019-02-24 04:46:54 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,12 @@
 # loading dataset file
 hpc <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
+hpc$Date <- strptime(paste(ECP$Date,ECP$Time),"%d/%m/%Y %H:%M:%S")
 
 # Subsetting the data with desired records
-SelData <- subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+SelData=subset(ECP,(ECP$Date>=strptime("2007-02-01","%Y-%m-%d")&(ECP$Date<strptime("2007-02-02","%Y-%m-%d"))))
 rm(hpc)
+
 
 ## Converting dates
 hello_hpc <- paste(as.Date(SelData$Date), SelData$Time)
@@ -14,6 +15,6 @@ SelData$Datetime <- as.POSIXct(hello_hpc)
 ## Create Histograms (Plot 1)
 hist(SelData$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-## png file creation
+## Saving to png file
 dev.copy(png, file="./data/plot1.png", height=480, width=480)
 dev.off()
