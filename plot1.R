fc3976a99c6862fab0fd8e8d6e1f5713760b041e
ycheng10 <yinhahelen@yahoo.com>
2019-02-24 04:26:11 -0500

    add plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# loading dataset file
+hpc <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
+
+# Subsetting the data with desired records
+SelData <- subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(hpc)
+
+## Converting dates
+hello_hpc <- paste(as.Date(SelData$Date), SelData$Time)
+SelData$Datetime <- as.POSIXct(hello_hpc)
+
+## Create Histograms (Plot 1)
+hist(SelData$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to png file
+dev.copy(png, file="./data/plot1.png", height=480, width=480)
+dev.off()
