dd306ac91e0a0009a34e4bae4d1dbbe34973c63c
ycheng10 <yinhahelen@yahoo.com>
2019-02-24 05:50:17 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -12,17 +12,16 @@ rm(hpc)
 hello_hpc <- paste(as.Date(SelData$Date), SelData$Time)
 SelData$Datetime <- as.POSIXct(hello_hpc)
 
-## Creating Plot 3
-plot(SelData$Global_active_power~SelData$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+## Creating Plot 3 (PNG)
+png(filename="./data/plot3.png",width=480,height=480)
 with(SelData, {
         plot(Sub_metering_1~Datetime, type="l",
-             ylab="Global Active Power (kilowatts)", xlab="")
+             ylab="Energy Sub metering", xlab="")
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
         })
-        legend("topright", lwd=1,col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        legend("topright", lwd=2,lty=1,col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-
-## Saving to png file
-dev.copy(png, file="./data/plot3.png", height=480, width=480)
 dev.off()
+
+
