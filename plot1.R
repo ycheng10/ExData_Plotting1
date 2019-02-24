6a20295da98cb08c8cd0ca2afe1d6eac900e1a00
ycheng10 <yinhahelen@yahoo.com>
2019-02-24 05:16:23 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,10 @@
 # loading dataset file
 hpc <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-hpc$Date <- strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 
 # Subsetting the data with desired records
-SelData=subset(hpc,(hpc$Date>=strptime("2007-02-01","%Y-%m-%d")&(hpc$Date<strptime("2007-02-02","%Y-%m-%d"))))
+SelData=subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(hpc)
 
 
